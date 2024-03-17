c758fa59ea9793636f0f804879354755180ebb98
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-17 13:39:47 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+// Custom Routing
+
+// Instances
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+// API Endpoints
+
+// app.use("api/v1/")
+
+const port = process.env.PORT || 9000;
+
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on PORT: ${port}...`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
