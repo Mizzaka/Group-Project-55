4b426ab3a81a35dc603090e067c36485f1e5f94f
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-27 14:12:41 +0530

    ui

--- adminPanel/src/pages/Blog.jsx
+++ adminPanel/src/pages/Blog.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import BlogTable from "../components/tables/BlogTable";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <main>
+      <Tabs variant="soft-rounded" colorScheme="green">
+        <div className="mt-10 flex justify-center">
+          <TabList>
+            <Tab>Existing Blog's</Tab>
+            <Tab>Create a Blog</Tab>
+          </TabList>
+        </div>
+        <TabPanels>
+          <TabPanel>
+            <Box border='1px' borderColor='gray.200' borderRadius={"lg"} boxShadow={"md"}>            
+                <BlogTable />
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <p>Create a Blog</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </main>
+  );
 };
 
 export default Blog;
