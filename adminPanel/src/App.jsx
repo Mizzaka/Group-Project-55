4428ce9b3f371f0b2ffaa884f3d9e4dbe224bf4d
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-29 02:13:50 +0530

    ui

--- adminPanel/src/App.jsx
+++ adminPanel/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navigation/Navbar";
 import DashboardPage from "./pages/DashboardPage";
 import Blog from "./pages/Blog";
 import Jobs from "./pages/Jobs";
+import LogIn from "./pages/LogIn";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<DashboardPage />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/jobs" element={<Jobs />} />
+          <Route path="/login" element={<LogIn />} />
         </Routes>
       </BrowserRouter>
     </>
