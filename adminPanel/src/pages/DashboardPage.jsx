159f99f3eafd54a446d13f4c46d7d532d23c728a
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-31 19:27:36 +0530

    ui

--- adminPanel/src/pages/DashboardPage.jsx
+++ adminPanel/src/pages/DashboardPage.jsx
@@ -55,32 +55,6 @@ const DashboardPage = () => {
     fetchFeedData();
   }, []);
 
-  const data = [
-    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-    { name: "Page A", uv: 500, pv: 400, amt: 4800 },
-    { name: "Page A", uv: 200, pv: 800, amt: 8400 },
-  ];
-
-  const data01 = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
-  const data02 = [
-    { name: "A1", value: 100 },
-    { name: "A2", value: 300 },
-    { name: "B1", value: 100 },
-    { name: "B2", value: 80 },
-    { name: "B3", value: 40 },
-    { name: "B4", value: 30 },
-    { name: "B5", value: 50 },
-    { name: "C1", value: 100 },
-    { name: "C2", value: 200 },
-    { name: "D1", value: 150 },
-    { name: "D2", value: 50 },
-  ];
-
   const chartFeedData = feedData;
 
   return (
